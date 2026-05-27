d988f0059cc0380c0443bffd5b258f64ce8d4dff
Nmarc <noemi.marcolini@gmail.com>
2026-05-27 21:06:39 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -58,6 +58,7 @@ const App = (() => {
       case 'home':      renderHomePage(); break;
       case 'campagna':  renderCampaignPage(); break;
       case 'mondo':     if (window.NPC) NPC.init(); break;
+      case 'compendio': if (window.Compendio) Compendio.init(); break;
       case 'sessione':  if (window.Sessione) Sessione.init(); break;
       case 'generatori': if (window.Generatori) Generatori.init(); break;
       case 'compendio': if (window.Compendio) Compendio.init(); break;
